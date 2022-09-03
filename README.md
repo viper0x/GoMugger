107ad24fb19861c8883adb4f5077ff162b7f7b94
viper0x <43915187+viper0x@users.noreply.github.com>
2022-09-03 12:10:14 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,95 @@
+# GoMugger
+
+![text-1662149638843](https://user-images.githubusercontent.com/43915187/188277728-fefe46d4-6848-4cce-aaee-115037648b7d.png)
+
+A fast tool written in Golang used to check for sensitive/juicy information within web pages content.
+The regexes list will be updated whenever I find/community shares interesting regexes.
+There's a very good ready regexes file and most of them has been collected by me from different repositories (big shout-out to the people who created/posted them).
+
+## Installation
+Using Go ([Go 1.16+ compiler](https://golang.org/doc/install) should be installed & configured!):
+```
+$ go get -u github.com/viper0x/gomugger
+```
+If the above didn't work:
+```
+$ go install github.com/viper0x/gomugger@latest
+```
+
+Or by manual building:
+```
+$ git clone https://github.com/viper0x/gomugger
+$ cd gomugger
+$ go build .
+$ cat targets.txt | ./gomugger
+```
+
+## Usage
+```
+$ gomugger -h
+
+   ______      __  ___                           
+  / ____/___  /  |/  /_  ______ _____ ____  _____
+ / / __/ __ \/ /|_/ / / / / __ \/ __ \/ _ \/ ___/
+/ /_/ / /_/ / /  / / /_/ / /_/ / /_/ /  __/ /    
+\____/\____/_/  /_/\__,_/\__, /\__, /\___/_/     
+                        /____//____/                                                                          
+
+  https://github.com/viper0x
+
+Usage:
+  [stdin] | gomugger [options]
+
+Options:
+  -c, --concurrency <val>     The concurrency level (default 25)
+  -s, --silent                Show results only without printing banner
+  -r, --regex <regex>         Use custom regex instead of using regex.json list
+  -rL, --regex-list <file>    Use another json regex list instead of regex.json (default regex.json)
+  -h, --help                  Display help
+
+Examples:
+  cat targets.txt | gomugger
+  echo https://example.com | gomugger
+  echo https://example.com | waybackurls | gomugger
+  cat targets.txt | gomugger -r <REGEX>
+  cat targets.txt | gomugger -rL <FILE.json>
+```
+
+## Concurrency
+You can set the concurrency level with the `-c` flag:
+```
+$ cat targets.txt | gomugger -c 50
+```
+
+## Append/Use different regexes
+### Appending new regex:
+You only have to edit the `regex.json` file and add your regex using the following format:
+```
+{
+ "name": "<NEW REGEX NAME>",
+ "regex": "<REGEX>"
+}
+```
+
+### Using custom regex against targets:
+```
+$ cat targets.txt | gomugger -r "REGEX"
+```
+
+### Using different regexes list:
+Create `.json` file and add regexes using the following format. Then you can use `-rL` followed by the json file name to use the custom created list:
+```
+{
+  "regexes": [
+    {
+      "name": "<REGEX NAME>",
+      "regex": "<REGEX>"
+    },
+		
+    {
+      "name": "<REGEX NAME>",
+      "regex": "<REGEX>"
+    }
+  ]
+}
+```
---
+++ main.go
@@ -0,0 +1,156 @@
+package main
+
+import(
+	"fmt"
+	"flag"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"github.com/fatih/color"
+	"sync"
+	"bufio"
+	"regexp"
+	"time"
+	"strings"
+)
+
+type Regexes struct {
+    Regexes []Regex `json:"regexes"`
+}
+
+type Regex struct {
+   Name string `json:"name"`
+   Regex string `json:"regex"`
+}
+
+var concurrency int
+var silent bool
+var outputDir string
+var customReg string
+var regList string
+
+func Banner() {
+	fmt.Println(`
+   ______      __  ___                           
+  / ____/___  /  |/  /_  ______ _____ ____  _____
+ / / __/ __ \/ /|_/ / / / / __ \/ __ \/ _ \/ ___/
+/ /_/ / /_/ / /  / / /_/ / /_/ / /_/ /  __/ /    
+\____/\____/_/  /_/\__,_/\__, /\__, /\___/_/     
+                        /____//____/                                                                          
+`)
+	fmt.Println("  https://github.com/viper0x\n")
+}
+
+
+func parseArguments() {
+    flag.IntVar(&concurrency, "concurrency", 25, "The concurrency level")
+    flag.IntVar(&concurrency, "c", 25, "The concurrency level")
+
+	flag.BoolVar(&silent, "silent", false, "Show results only without printing banner")
+	flag.BoolVar(&silent, "s", false, "Show results only without printing banner")
+
+	flag.StringVar(&customReg, "regex", customReg, "Use custom regex instead of using regex.json list")
+	flag.StringVar(&customReg, "r", customReg, "Use custom regex instead of using regex.json list")
+
+	flag.StringVar(&regList, "rL", "regex.json", "Use another json regex list instead of regex.json")
+	flag.StringVar(&regList, "regex-list", "regex.json", "Use another json regex list instead of regex.json")
+
+
+    flag.Parse()
+}
+
+func matchContent(name string, regex string, target string, content string) {
+	re1, _ := regexp.Compile(regex)
+	match := re1.FindString(content)
+
+	if match != "" {
+		fmt.Printf("[%s] [%s] %s [%s]\n", color.CyanString(time.Now().Format("01-02-2006 15:04:05")), color.GreenString(name), target, color.YellowString(match))
+	}
+    
+}
+
+func main() {
+
+	flag.Usage = func() {
+		h := []string{
+			"Usage:",
+			"  [stdin] | gomugger [options]\n",
+			"Options:",
+			"  -c, --concurrency <val>     The concurrency level (default 25)",
+			"  -s, --silent                Show results only without printing banner",
+			"  -r, --regex <regex>         Use custom regex instead of using regex.json list",
+			"  -rL, --regex-list <file>    Use another json regex list instead of regex.json (default regex.json)",
+			"  -h, --help                  Display help\n",
+			"Examples:",
+			"  cat targets.txt | gomugger",
+			"  echo https://example.com | gomugger",
+			"  echo https://example.com | waybackurls | gomugger",
+			"  cat targets.txt | gomugger -r <REGEX>",
+			"  cat targets.txt | gomugger -rL <FILE.json>\n",
+		}
+		Banner()
+		fmt.Fprintf(os.Stderr, "%s", strings.Join(h, "\n"))
+	}
+
+	parseArguments()
+
+	if !silent {
+		Banner()
+	}
+
+	jobs := make(chan string)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+
+		go func() {
+
+			for target := range jobs {
+				response, err := http.Get(target)
+
+				if err != nil {
+					continue
+				}
+
+				content, _ := ioutil.ReadAll(response.Body)
+
+				if customReg != "" {
+				matchContent("Custom Regex", customReg, target, string(content))
+
+				} else {
+
+					file, err := os.Open(regList)
+
+					if err != nil {
+						fmt.Println(err)
+					}
+
+					defer file.Close()
+
+					byteResult, _ := ioutil.ReadAll(file)
+
+					var regexes Regexes
+
+					json.Unmarshal(byteResult, &regexes)
+
+					for i := 0; i < len(regexes.Regexes); i++ {
+						matchContent(regexes.Regexes[i].Name, regexes.Regexes[i].Regex, target, string(content))
+					}
+				}
+			}
+
+			wg.Done()
+		}()
+	}
+
+	sc := bufio.NewScanner(os.Stdin)
+        for sc.Scan() {
+                jobs <- sc.Text()
+        }
+
+        close(jobs)
+		wg.Wait()
+}
